1983c1f22aa95bdc1e9e1dc53d1ff5fa75b94264
masterdude <xmertsahin@gmail.com>
2023-02-10 14:15:26 +0300

    Added Ui and Ui Related Game Logic

--- script.js
+++ script.js
@@ -1,46 +1,116 @@
 let playerScore = 0;
 let cpuScore = 0;
+const playerButtons = document.querySelectorAll(".player-wrapper > a");
+const cpuButtons = document.querySelectorAll(".cpu-wrapper > a");
+let cpuSelectedButton = null;
+let playerSelectedButton = null;
+const playerScoreHeader = document.querySelector(".player-section>h1");
+const cpuScoreHeader = document.querySelector(".cpu-section>h1");
+const gameOverModal = document.querySelector(".modal");
+const gameOverModalText = document.querySelector(".modal>h1");
 
 function getCpuChoice(){
-    let selectionList = ["rock", "paper", "scissor"]
-    let cpuSelected = selectionList[Math.floor(Math.random() * selectionList.length)];
-    alert("cpu chooses " + cpuSelected);
+    
+    let cpuMove = cpuButtons[Math.floor(Math.random() * cpuButtons.length)]
+    let cpuSelected= cpuMove.className;
+    console.log(typeof(cpuSelected));
+    console.log(cpuSelected);
+    changeColor(cpuMove);
     return cpuSelected;
 }
 
-function getPlayerChoice() {
-    let choice = prompt("choose rock, paper, scissor");
-    let choiceLowerCase = choice.toLowerCase();
-    alert("you choose " + choiceLowerCase);
-    return choiceLowerCase;
+  for (let index = 0; index < playerButtons.length; index++) {
+        playerButtons[index].addEventListener("click", function(){
+            removeClass();
+            let playerSelection = this.className;
+            console.log(typeof(playerSelection) );
+            console.log(playerSelection);
+            playRound(playerSelection, getCpuChoice())
+            changeColor(this);
+        });
+        
+    }
+
+
+    function changeColor(elementToChange){
+        
+        let element = elementToChange;
+        element.classList.add("selected");
+    }
+//function getPlayerChoice() {
     
-}
+    //for (let index = 0; index < playerButtons.length; index++) {
+       // playerButtons[index].addEventListener("click", function(){
+            //return
+      //  });
+        
+   // }
+
+    //let choice = prompt("choose rock, paper, scissor");
+    //let choiceLowerCase = choice.toLowerCase();
+    //alert("you choose " + choiceLowerCase);
+    // return choiceLowerCase;
+    
+//}
 
 function playRound(playerchoice, cpuchoice){
     
-    if (playerchoice === "rock" && cpuchoice ==="scissor" || 
-    playerchoice === "paper" && cpuchoice ==="rock" ||
-    playerchoice === "scissor" && cpuchoice ==="paper" ) {
-        alert("you win");
-        playerScore = playerScore + 1;
+    if (playerchoice === `player-rock` && cpuchoice ===`cpu-scissors` || 
+    playerchoice === "player-paper" && cpuchoice === "cpu-rock" ||
+    playerchoice === "player-scissors" && cpuchoice === "cpu-paper" ) {
         
-    } else if(playerchoice === cpuchoice){
-        alert ("draw, go again!");
+        playerScore = playerScore + 1;
+        updateScoreText(playerScoreHeader, "YOU", playerScore);
+        checkGameEnd(playerScore, cpuScore);
+        
+    } else if(playerchoice === `player-rock` && cpuchoice ===`cpu-rock` || 
+    playerchoice === "player-paper" && cpuchoice === "cpu-paper" ||
+    playerchoice === "player-scissors" && cpuchoice === "cpu-scissors" ){
+        
         
     } else {
-        alert("you lose!")
+      
         cpuScore = cpuScore + 1;
-    }
-
-}
-
-function playGame(){
-    for (let index = 0; index < 5; index++) {
-        playRound(getPlayerChoice(), getCpuChoice());
-        console.log(cpuScore , playerScore);
+        updateScoreText(cpuScoreHeader, "CPU", cpuScore);
+        checkGameEnd(playerScore, cpuScore);
         
     }
-    alert("your score " + playerScore);
-    alert("cpu score " + cpuScore );
+
+    
+
 }
+
+function removeClass(){
+    let coloredButtons = document.querySelectorAll(".selected");
+    coloredButtons.forEach(element => {
+        element.classList.remove("selected");
+    });
+}
+
+function updateScoreText(textElement, who, score){
+    textElement.innerText = `${who}: ${score}`;
+}
+
+
+function checkGameEnd(playerScore, cpuScore){
+    if (playerScore == 5) {
+        gameOverModalText.innerText = "YOU WIN! YAAAAY :)";
+        gameOverModal.classList.add("modal-display");
+    }else if(cpuScore == 5){
+        
+        gameOverModalText.innerText = "YOU LOSE! POOPIE :(";
+        gameOverModal.classList.add("modal-display");
+    }
+}
+
+
+//function playGame(){
+    //for (let index = 0; index < 5; index++) {
+       // playRound(getPlayerChoice(), getCpuChoice());
+       // console.log(cpuScore , playerScore);
+        
+   // }
+   // alert("your score " + playerScore);
+    //alert("cpu score " + cpuScore );
+//}
 
